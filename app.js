122ad266b2440fc01f76a80c38a9fb65b364149c
Theo <theodorosladasmail@gmail.com>
2022-04-12 22:53:25 +0100

    Added - Point functionality

--- app.js
+++ app.js
@@ -1,51 +1,113 @@
 // SELECTORS
-// const exercise1 = {
-//   title: "Front Plank",
-//   image: document.getElementsByClassName(".exercise-image").src="1.jpg",
-//   score1: 1,
-//   span1: '0s - 120s',
-//   score2: 1++,
-//   span2: '121s - 210s',
-//   score3: 2++,
-//   span3: '211s+',
-//   tip: document.getElementsByClassName(".tip-text"),
-//   score: 0
-// };
-// const exercise2 = {
-//   title: "Second Plank",
-//   image: document.getElementsByClassName(".exercise-image").src="2.jpg",
-//   score1: 1,
-//   span1: '0s - 30s',
-//   score2: 1++,
-//   span2: '31s - 40s',
-//   score3: 2++,
-//   span3: '41s+',
-//   tip: document.getElementsByClassName(".tip-text"),
-//   score: 0
-// };
-const nextBtn = document.querySelector(".next");
-const score = document.querySelector(".score-value");
-// const score1 = 1;
-// const score2 = 1++;
-// const score3 = 2++;
-// const score1Btn = document.querySelector('.score1');
-// const score2Btn = document.querySelector('.score2');
-// const score3Btn = document.querySelector('.score3');
+
+// function data() {
+//   return {
+//     data: [
+//       {
+//         name: "Front Plank",
+//         score_1: "0-120 secs",
+//         score_2: "121-210 secs",
+//         score_3: "211+ secs ",
+//         tip: "Engage your core and make sure you stay in a straight line.",
+//         img_url: "./img/1image-front-plank.jpg",
+//       },
+//       {
+//         name: "Side Plank",
+//         score_1: "0-30 secs",
+//         score_2: "31-60 secs",
+//         score_3: "61-90 secs",
+//         tip: "Keep your elbow directly underneath your shoulder. Keep your hips, knees and ankles in line, lifting your hip as much as possible.",
+//         img_url: "./img/2image-side-plank.jpg",
+//       },
+//       {
+//         name: "Single Leg Sit to Stand",
+//         score_1: "0-10 reps",
+//         score_2: "11-20 reps",
+//         score_3: "21-30 reps",
+//         tip: "Make sure your knee is kept in control. Drive upwards with your glutes.",
+//         img_url: "./img/3image-single-leg-sit.jpg",
+//       },
+//       {
+//         name: "Press Up",
+//         score_1: "0-20 reps",
+//         score_2: "21-40 reps",
+//         score_3: "40 reps+",
+//         tip: "Keep your body up as one. Stay in a nice straight line, as if you were doing a plank.",
+//         img_url: "./img/4image-press-up.jpg",
+//       },
+//       {
+//         name: "Double Leg Wall Sit and Hold",
+//         score_1: "0-30 secs",
+//         score_2: "31-60 secs",
+//         score_3: "61-90 secs",
+//         tip: "Squat against a wall with your legs at a 90 degree angle. Stay nice and low, focusing on quality over quantity. ",
+//         img_url: "./img/5image-double-leg-sit.jpg",
+//       },
+//     ],
+//     score: 0,
+//     index: 0,
+//     clicked: false,
+//     results: false,
+//   };
+// }
+const exercise1 = {
+  name: "Front Plank",
+  score_1: "0-120 secs",
+  score_2: "121-210 secs",
+  score_3: "211+ secs ",
+  tip: "Engage your core and make sure you stay in a straight line.",
+  img_url: "./img/1image-front-plank.jpg",
+  score: 0,
+};
+
+const nextBtn = document.querySelector(".next-btn");
+const pointBtn1 = document.querySelector(".point-btn1");
+const pointBtn2 = document.querySelector(".point-btn2");
+const pointBtn3 = document.querySelector(".point-btn3");
+const spanScore = document.querySelector(".score");
+let totalScore = 0;
+pointBtn1.addEventListener("click", () => {
+  addOnePoint();
+});
+pointBtn2.addEventListener("click", () => {
+  addTwoPoints();
+});
+pointBtn3.addEventListener("click", () => {
+  addThreePoints();
+});
 
 // FUNCTIONS
-function exercise2() {
-  const title = (document.querySelector(".exercise-title").innerHTML =
-    "BACK PLANK");
-  const image = (document.getElementById("exercice-img").src = "2.jpg");
-  const score1Span = (document.querySelector(".score-1-span").innerHTML =
-    "test");
-  const score2Span = (document.querySelector(".score-2-span").innerHTML =
-    "test2");
-  const score3Span = (document.querySelector(".score-3-span").innerHTML =
-    "test3");
+function addOnePoint() {
+  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 1)}`;
+  this.clicked = true;
+}
+// add two points function
+function addTwoPoints() {
+  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 2)}`;
+  this.clicked = true;
+}
+// add three points function
+function addThreePoints() {
+  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 3)}`;
+  this.clicked = true;
 }
 
+// προχειρο
+
+// function exercise2() {
+//   const title = (document.querySelector(".exercise-title").innerHTML =
+//     "SIDE PLANK (LEFT & RIGHT SIDES)");
+//   const image = (document.getElementById("exercice-img").src =
+//     "/img/2image-side-plank.jpg");
+//   const score1Span = (document.querySelector(".point-1-span").innerHTML =
+//     "0-30 secs");
+//   const score2Span = (document.querySelector(".point-2-span").innerHTML =
+//     "31-60 secs");
+//   const score3Span = (document.querySelector(".point-3-span").innerHTML =
+//     "61-90 secs");
+// }
+
 // EVENT LISTENERS
-nextBtn.addEventListener("click", () => {
-  exercise2();
-});
+// nextBtn.addEventListener("click", () => {
+//   // exercise2();
+// });
