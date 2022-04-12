bbb81898ce7fb2b4768728ae7dc47cad75cda75d
Theo <theodorosladasmail@gmail.com>
2022-04-12 11:59:12 +0100

    Added - JS selectors

---
+++ app.js
@@ -0,0 +1,30 @@
+// SELECTORS
+const exercise1 = {
+  title: "Front Plank",
+  image: document.getElementsByClassName(".exercise-image").src="1.jpg",
+  score1: 1,
+  span1: '0s - 120s',
+  score2: 1++,
+  span2: '121s - 210s',
+  score3: 2++,
+  span3: '211s+',
+  tip: document.getElementsByClassName(".tip-text"),
+  score: 0
+};
+const exercise2 = {
+  title: "Second Plank",
+  image: document.getElementsByClassName(".exercise-image").src="2.jpg",
+  score1: 1,
+  span1: '0s - 30s',
+  score2: 1++,
+  span2: '31s - 40s',
+  score3: 2++,
+  span3: '41s+',
+  tip: document.getElementsByClassName(".tip-text"),
+  score: 0
+};
+
+const nextBtn = document.querySelector('.next');
+const score1 = document.querySelector('.score1');
+// FUNCTIONS
+
