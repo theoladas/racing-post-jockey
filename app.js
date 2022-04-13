cf24e0f6f4078a18ec706028dc57c728792073bb
Theo <theodorosladasmail@gmail.com>
2022-04-13 13:19:40 +0100

    Added - exercise buttons

--- app.js
+++ app.js
@@ -1,4 +1,94 @@
 // SELECTORS
+// next exercise btn
+const nextBtn = document.querySelector(".next-btn");
+nextBtn.disabled = true;
+// point btns
+const pointBtn1 = document.querySelector(".point-btn1");
+const pointBtn2 = document.querySelector(".point-btn2");
+const pointBtn3 = document.querySelector(".point-btn3");
+// display user's score
+const spanScore = document.querySelector(".score");
+
+// FUNCTIONS
+// all point btns enabled
+function pointBtnsEnabled() {
+  pointBtn1.disabled = false;
+  pointBtn2.disabled = false;
+  pointBtn3.disabled = false;
+}
+// all point btns disabled
+function pointBtnsDisabled() {
+  pointBtn1.disabled = true;
+  pointBtn2.disabled = true;
+  pointBtn3.disabled = true;
+}
+// add one point in user's display score
+function addOnePoint() {
+  let totalScore = (spanScore.innerHTML = `${(this.spanScore =
+    exercise1.score + 1)}`);
+  nextBtn.disabled = false;
+  pointBtnsDisabled();
+}
+// add two points in user's display score
+function addTwoPoints() {
+  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 2)}`;
+  nextBtn.disabled = false;
+  pointBtnsDisabled();
+}
+// add three points in user's display score
+function addThreePoints() {
+  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 3)}`;
+  nextBtn.disabled = false;
+  pointBtnsDisabled();
+}
+// exercise 2 stats
+function exercise2() {
+  const title = (document.querySelector(".exercise-title").innerHTML =
+    "SIDE PLANK (LEFT & RIGHT SIDES)");
+  const image = (document.getElementById("exercice-img").src =
+    "/img/2image-side-plank.jpg");
+  const point1 = (document.querySelector(".point-1-span").innerHTML =
+    "0-30 secs");
+  const point2 = (document.querySelector(".point-2-span").innerHTML =
+    "31-60 secs");
+  const point3 = (document.querySelector(".point-3-span").innerHTML =
+    "61-90 secs");
+  const tip = (document.querySelector(".tip").innerHTML =
+    "TIP: Keep your elbow directly underneath your shoulder. Keep your hips, knees and ankles in line, lifting your hip as much as possible.");
+  pointBtnsEnabled();
+  nextBtn.disabled = true;
+  // const newExerciseBtn = document.createElement("button");
+  // newExerciseBtn.innerText = "Next to Exercise-3";
+  // newExerciseBtn.innerText = "Next to Exercise-3";
+}
+
+// DATA
+const exercise1 = {
+  name: "Front Plank",
+  score_1: "0-120 secs",
+  score_2: "121-210 secs",
+  score_3: "211+ secs ",
+  tip: "Engage your core and make sure you stay in a straight line.",
+  img_url: "./img/1image-front-plank.jpg",
+  score: 0,
+};
+
+// EVENT LISTENERS
+pointBtn1.addEventListener("click", () => {
+  addOnePoint();
+});
+pointBtn2.addEventListener("click", () => {
+  addTwoPoints();
+});
+pointBtn3.addEventListener("click", () => {
+  addThreePoints();
+});
+nextBtn.addEventListener("click", () => {
+  exercise2();
+});
+nextBtn.addEventListener("click", () => {
+  exercise2();
+});
 
 // function data() {
 //   return {
@@ -50,138 +140,60 @@
 //     results: false,
 //   };
 // }
-const exercise1 = {
-  name: "Front Plank",
-  score_1: "0-120 secs",
-  score_2: "121-210 secs",
-  score_3: "211+ secs ",
-  tip: "Engage your core and make sure you stay in a straight line.",
-  img_url: "./img/1image-front-plank.jpg",
-  score: 0,
-};
 
-const nextBtn = document.querySelector(".next-btn");
-nextBtn.disabled = true;
-const pointBtn1 = document.querySelector(".point-btn1");
-const pointBtn2 = document.querySelector(".point-btn2");
-const pointBtn3 = document.querySelector(".point-btn3");
-const spanScore = document.querySelector(".score");
-let totalScore = 0;
-// FUNCTIONS
-function pointBtnsDisabled() {
-  pointBtn1.disabled = true;
-  pointBtn2.disabled = true;
-  pointBtn3.disabled = true;
-}
-function addOnePoint() {
-  let totalScore = (spanScore.innerHTML = `${(this.spanScore =
-    exercise1.score + 1)}`);
-  nextBtn.disabled = false;
-  pointBtnsDisabled();
-}
-// add two points function
-function addTwoPoints() {
-  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 2)}`;
-  nextBtn.disabled = false;
-  pointBtnsDisabled();
-}
-// add three points function
-function addThreePoints() {
-  spanScore.innerHTML = `${(this.spanScore = exercise1.score + 3)}`;
-  nextBtn.disabled = false;
-  pointBtnsDisabled();
-}
-
-// EVENT LISTENERS
-pointBtn1.addEventListener("click", () => {
-  addOnePoint();
-});
-pointBtn2.addEventListener("click", () => {
-  addTwoPoints();
-});
-pointBtn3.addEventListener("click", () => {
-  addThreePoints();
-});
-
-nextBtn.addEventListener("click", () => {
-  alert("test");
-});
-// προχειρο
-
-// function exercise2() {
-//   const title = (document.querySelector(".exercise-title").innerHTML =
-//     "SIDE PLANK (LEFT & RIGHT SIDES)");
-//   const image = (document.getElementById("exercice-img").src =
-//     "/img/2image-side-plank.jpg");
-//   const score1Span = (document.querySelector(".point-1-span").innerHTML =
-//     "0-30 secs");
-//   const score2Span = (document.querySelector(".point-2-span").innerHTML =
-//     "31-60 secs");
-//   const score3Span = (document.querySelector(".point-3-span").innerHTML =
-//     "61-90 secs");
-// }
-
-// EVENT LISTENERS
-// nextBtn.addEventListener("click", () => {
-//   // exercise2();
-// });
-
-// VUE
-{
-  /* <div v-for="(d, i) in data" :key="'data-' + i" class="col-12">
-          <!-- index page -->
-          <div v-if="i == index">
-            <!-- image src from data -->
-            <img style="align-items: center" :src="d.img_url" :alt="d.name" />
-            <div class="col-12 d-flex" style="justify-content: space-between">
-              <div></div>
-              <div class="d-flex">
-                <!-- button 1 -->
-                <div class="point__card" @click="addOnePoint()">
-                  <h3 class="mb-0">1 Point</h3>
-                  <p class="mb-0">
-                    {{ d.score_1 }}
-                  </p>
-                </div>
-                <!-- button 2 -->
-                <div class="point__card" @click="addTwoPoints()">
-                  <h3 class="mb-0">2 Points</h3>
-                  <p class="mb-0">
-                    {{ d.score_2 }}
-                  </p>
-                </div>
-                <div class="point__card" @click="addThreePoints()">
-                  <h3 class="mb-0">3 Point</h3>
-                  <p class="mb-0">
-                    {{ d.score_3 }}
-                  </p>
-                </div>
-              </div>
-              <div>
-                <button :disabled="!clicked" v-if="i < 4" @click="nextSlide()">
-                  Next
-                </button>
-                <button
-                  v-if="i == 4"
-                  :disabled="!clicked"
-                  @click="results = true"
-                >
-                  See results
-                </button>
-              </div>
-            </div>
-            <div class="col-12 d-flex" style="justify-content: center">
-              <h5 class="mt-4"><span>TIP: </span>{{ d.tip }}</h5>
-            </div>
+/* <div v-for="(d, i) in data" :key="'data-' + i" class="col-12">
+    <!-- index page -->
+    <div v-if="i == index">
+      <!-- image src from data -->
+      <img style="align-items: center" :src="d.img_url" :alt="d.name" />
+      <div class="col-12 d-flex" style="justify-content: space-between">
+        <div></div>
+        <div class="d-flex">
+          <!-- button 1 -->
+          <div class="point__card" @click="addOnePoint()">
+            <h3 class="mb-0">1 Point</h3>
+            <p class="mb-0">
+              {{ d.score_1 }}
+            </p>
+          </div>
+          <!-- button 2 -->
+          <div class="point__card" @click="addTwoPoints()">
+            <h3 class="mb-0">2 Points</h3>
+            <p class="mb-0">
+              {{ d.score_2 }}
+            </p>
+          </div>
+          <div class="point__card" @click="addThreePoints()">
+            <h3 class="mb-0">3 Point</h3>
+            <p class="mb-0">
+              {{ d.score_3 }}
+            </p>
           </div>
         </div>
+        <div>
+          <button :disabled="!clicked" v-if="i < 4" @click="nextSlide()">
+            Next
+          </button>
+          <button
+            v-if="i == 4"
+            :disabled="!clicked"
+            @click="results = true"
+          >
+            See results
+          </button>
+        </div>
+      </div>
+      <div class="col-12 d-flex" style="justify-content: center">
+        <h5 class="mt-4"><span>TIP: </span>{{ d.tip }}</h5>
       </div>
     </div>
+  </div>
+</div>
+  </div>
     <div
       class="container min-vh-100 min-vw-100"
       style="align-items: center; justify-content: center; display: grid"
-      v-if="results"
-    >
+      v-if="results">
       <div>
         <h5>Your score is: {{ score }}</h5>
         <h1>
@@ -295,4 +307,3 @@ export default {
   },
 };
 </script> */
-}
